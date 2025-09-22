7185f3ddb3a1cce13d0b9145c5459f9b4b3795a3
Anton <anton.delitsch@implema.se>
2025-09-22 10:50:51 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Run CPILint as part of pipeline
+FROM azul/zulu-openjdk:22
+
+WORKDIR /
+
+# Install dependencies
+RUN apt-get update \
+ && apt-get install -y unzip=6.0-26ubuntu3.2 wget=1.21.2-2ubuntu1 zip=3.0-12build2 --no-install-recommends \
+ && apt-get clean \
+ && rm -rf /var/lib/apt/lists/*
+
+COPY cpilint-1.1.0.zip .
+
+# Prepare installation
+RUN mkdir -p tmp
+RUN unzip cpilint-*.zip -d tmp
+
+RUN mv tmp/cpilint* cpilint
+
+RUN chmod +x cpilint/bin/cpilint
+
+COPY pipe /
+COPY LICENSE.txt pipe.yml README.md /
+RUN wget --progress=dot:giga -P / https://bitbucket.org/bitbucketpipelines/bitbucket-pipes-toolkit-bash/raw/0.6.0/common.sh
+
+RUN chmod a+x /*.sh
+
+ENTRYPOINT ["/pipe.sh"]
---
+++ pipe/pipe.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+#
+# This pipe is an example to show how easy is to create pipes for Bitbucket Pipelines.
+#
+
+source "$(dirname "$0")/common.sh"
+
+info "Executing the pipe..."
+
+# Required parameters
+RULES=${RULES:?'RULES variable missing.'}
+
+
+# Default parameters
+DEBUG=${DEBUG:="false"}
+TYPE=${TYPE:='DIRECTORY'}
+CODE_INSIGHTS_RESULTS=${CODE_INSIGHTS_RESULTS:='false'}
+FILENAME=${FILENAME:='report.xml'}
+
+if [ "$TYPE" == "DIRECTORY" ]; then
+    cd $DIRECTORY
+    for i in *
+      do
+        if [ -d "$i" ]
+            then
+                info "Processing directory: $i"
+                cd $i
+                if [ -f "value_mapping.xml" ]; then
+                  info "Skipping value mapping: $i"
+                  cd ..
+                  continue
+                fi
+                zip -r ../"$i.zip" .
+                cd ..
+        fi
+      done
+    cd ..
+    run $(dirname "$0")/../cpilint/bin/cpilint -rules "$RULES" -directory "$DIRECTORY" -debug -boring -format junit -output "$FILENAME"
+else
+    run $(dirname "$0")/../cpilint/bin/cpilint -rules "$RULES" -host "$HOST" -username "$USERNAME" -password "$PASSWORD" -packages "$PACKAGES" -debug -boring -format junit -output "$FILENAME"
+fi
+
+if [[ "${status}" == "0" ]]; then
+  success "Success!"
+else
+  fail "Error!"
+fi
